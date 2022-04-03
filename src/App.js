a56cf3c335b888558c5df520d4e7bfaa40fa4b8f
mflanigan <mflanigantwualumn@gmail.com>
2022-04-03 15:06:28 -0500

    setting up header and navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import React from "react";
-import About from "./components/About";
+import React from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
   return (
-    <main>
-      <About></About>
-    </main>
+    <div>
+      <Header />
+      <Footer/>
+    </div>
   );
 }
 
---
+++ src/components/Footer/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Footer() {   
+    return (
+    <footer className="footer bg-dark fixed-bottom">
+        <a href="https://github.com/mflanigan13"><img src="" alt="Github" className="icon"/></a>
+        <a href="https://www.linkedin.com/in/mallorie-flanigan-54a32a224/"><img src="" alt="LinkedIn" className="icon"/></a>
+    </footer>
+    )
+}
+
+export default Footer;
