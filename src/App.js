514bf698200601e90689527f823ba086f355fda2
mflanigan <mflanigantwualumn@gmail.com>
2022-04-06 18:51:50 -0500

    set up app and portfolio container

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import PortfolioContainer from "./components/PortfolioContainer";
+import Footer from "./components/Footer";
+
+function App() {
+    return (
+        <header>
+            <PortfolioContainer />
+            <Footer />
+        </header>
+    )
+};
+
+export default App;
---
+++ src/components/PortfolioContainer.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import NavTabs from './NavTabs';
+import Home from './pages/Home';
+import About from './pages/About';
+import Projects from './pages/Projects';
+import Contact from './pages/Contact';
+
+export default function PortfolioContainer() {
+    const [currentPage, setCurrentPage] = useState('Home');
+
+    const renderPage = () => {
+        switch (currentPage) {
+            case 'Home':
+                return <Home />;
+            case 'About':
+                return <About />;
+            case 'Projects':
+                return <Projects />;
+            case 'Contact':
+                return <Contact />;
+            default:
+                console.log('default switch case has been triggered');
+        }
+
+    };
+
+    const handlePageChange = (page) => setCurrentPage(page);
+
+    return (
+        <div>
+            {/* We are passing the currentPage from state and the function to update it */}
+            <NavTabs currentPage={currentPage} handlePageChange={handlePageChange} />
+            {/* Here we are calling the renderPage method which will return a component  */}
+            {renderPage()}
+        </div>
+    );
+}
