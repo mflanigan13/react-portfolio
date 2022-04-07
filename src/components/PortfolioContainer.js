d15e9c71cff9b7c3bdee76530f9fd0af24b9bf4f
mflanigan <mflanigantwualumn@gmail.com>
2022-04-06 19:21:27 -0500

    got app working again

--- src/components/PortfolioContainer.js
+++ src/components/PortfolioContainer.js
@@ -1,10 +1,10 @@
 import React, { useState } from 'react';
-import NavTabs from './NavTabs';
+import Navbar from './Navbar';
 import Home from './pages/Home';
 import About from './pages/About';
 import Projects from './pages/Projects';
 import Contact from './pages/Contact';
-
+// TODO: FIXME:
 export default function PortfolioContainer() {
     const [currentPage, setCurrentPage] = useState('Home');
 
@@ -29,7 +29,7 @@ export default function PortfolioContainer() {
     return (
         <div>
             {/* We are passing the currentPage from state and the function to update it */}
-            <NavTabs currentPage={currentPage} handlePageChange={handlePageChange} />
+            <Navbar currentPage={currentPage} handlePageChange={handlePageChange} />
             {/* Here we are calling the renderPage method which will return a component  */}
             {renderPage()}
         </div>
